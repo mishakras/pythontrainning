2befcf9de50c15930cc7652e89987e02b42398f4
mishakras <irinabudtova59@gmail.com>
2021-08-27 12:48:49 +0300

    finding that 0 is a power of 2 bug

--- homework1/task1/sample_project/tests/test_calculator.py
+++ homework1/task1/sample_project/tests/test_calculator.py
@@ -11,3 +11,8 @@ def test_positive_case():
 def test_negative_case():
     """Testing that non-powers of 2 give False"""
     assert not check_power_of_2(12)
+
+
+def test_zero():
+    """Testing that zero gives False"""
+    assert not check_power_of_2(0)
