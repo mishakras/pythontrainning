e584c41a7801ef64ab440d354a40b1ce0ca3f1a5
mishakras <irinabudtova59@gmail.com>
2021-08-29 15:51:56 +0300

    tasks 4 fixed

--- homework1/task4.py
+++ homework1/task4.py
@@ -18,26 +18,20 @@ def check_sum_of_four(a: List[int], b: List[int], c: List[int],
     d.sort()
     n = len(a)
     if a[0] + b[0] + c[0] + d[0] > 0:
-        print("a")
         return 0
     if a[n-1] + b[n-1] + c[n-1] + d[n-1] < 0:
-        print("b")
         return 0
     for i in a:
         if -d[0] - b[0] - c[0] < i or i < -d[n-1] - b[n-1] - c[n-1]:
-            print("c")
             break
         for j in b:
             if -d[0] - c[0] < i + j or i + j < -d[n-1] - c[n-1]:
-                print("d")
                 break
             for k in c:
                 if -d[0] < i + j + k or i + j + k < -d[n - 1]:
-                    print("e")
                     break
                 for g in d:
                     if i + j + k + g > 0:
-                        print("k")
                         break
                     if i + j + k + g == 0:
                         amount = amount + 1
