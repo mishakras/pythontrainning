6bcf04a7c805b5c775764eeb98a611666049eadd
mishakras <irinabudtova59@gmail.com>
2021-09-01 18:19:29 +0300

    flacke8 fix

--- tests/homework2/test_task1.py
+++ tests/homework2/test_task1.py
@@ -1,7 +1,10 @@
 import os
 
-from homework2.task1 import count_non_ascii_chars, count_punctuation_chars, \
-    get_longest_diverse_words, get_most_common_non_ascii_char, get_rarest_char
+from homework2.task1 import count_non_ascii_chars
+from homework2.task1 import count_punctuation_chars
+from homework2.task1 import get_longest_diverse_words
+from homework2.task1 import get_most_common_non_ascii_char
+from homework2.task1 import get_rarest_char
 
 
 def test_rarest_char_positive():
