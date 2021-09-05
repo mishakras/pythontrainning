a3d0502414e14b6a0547b075c796b57306049cde
mishakras <irinabudtova59@gmail.com>
2021-09-05 12:34:58 +0300

    flake8 fix

--- homework3/task2.py
+++ homework3/task2.py
@@ -7,7 +7,7 @@ from multiprocessing import Process
 
 def slow_calculate(value):
     """Some weird voodoo magic calculations"""
-    time.sleep(random.randint(1,3))
+    time.sleep(random.randint(1, 3))
     data = hashlib.md5(str(value).encode()).digest()
     return sum(struct.unpack('<' + 'B' * len(data), data)), value
 
