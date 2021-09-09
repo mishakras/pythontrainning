6d6d52a8ba7432c57c7b461e49d4b2ed97e70764
mishakras <irinabudtova59@gmail.com>
2021-09-09 18:16:52 +0300

    task1 fix

--- homework/homework3/task1.py
+++ homework/homework3/task1.py
@@ -5,8 +5,11 @@ def cache(func: Callable, count: int) -> Callable:
     lst = []
 
     def fin(*args) -> Tuple[List, Any]:
+        for i in lst:
+            if i[0] == [*args]:
+                return i[1]
         a = func(*args)
         if len(lst) < count:
-            lst.append(a)
-        return lst, a
+            lst.append([[*args], a])
+        return a
     return fin
--- tests/homework3/test_task1.py
+++ tests/homework3/test_task1.py
@@ -1,27 +1,36 @@
+import time
+
 from homework.homework3.task1 import cache
 
 
-def func(a: int, b: int) -> bool:
-    return a+b > b*b
+def func(a: int, b: int) -> int:
+    return a + 2*b
 
 
-def func2(a: int, b: int, c: int) -> bool:
-    return a*c+b > c+b*b
+def func2(a: int) -> int:
+    time.sleep(1)
+    return a
 
 
-def test_1_run():
-    fin = cache(func, 2)
-    assert not fin(1, 2)[1]
+def test_check_of_base_func():
+    fin = cache(func, 1)
+    assert fin(1, 1) == 3
 
 
-def test_2_runs():
-    fin = cache(func, 2)
-    fin(1, 2)
-    assert fin(5, 1)[0] == [False, True]
+def test_check_of_cache():
+    fin = cache(func2, 1)
+    fin(5)
+    temp = time.time()
+    fin(5)
+    assert time.time() - temp < 1
 
 
-def test_more_runs():
+def test_check_of_cache_to_many_calls():
     fin = cache(func2, 2)
-    fin(1, 2, 3)
-    fin(1, 2, 10)
-    assert fin(5, 1, 1)[0] == [False, False]
+    fin(1)
+    fin(2)
+    fin(3)
+    fin(5)
+    temp = time.time()
+    fin(5)
+    assert time.time() - temp > 0.5
