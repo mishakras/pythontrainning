f1062160766a9c9a12470eb757cca35b0515aa5a
mishakras <irinabudtova59@gmail.com>
2021-09-15 18:08:39 +0300

    flake8 fix

--- homework/homework5/task2.py
+++ homework/homework5/task2.py
@@ -4,11 +4,13 @@
 исходную функцию в атрибуте __original_func
 print_result изменять нельзя, за исключением добавления вашего
 декоратора на строку отведенную под него - замените комментарий
-До применения вашего декоратор будет вызываться AttributeError при custom_sum.__original_func
+До применения вашего декоратор будет вызываться
+ AttributeError при custom_sum.__original_func
 Это корректное поведение
 После применения там должна быть исходная функция
 Ожидаемый результат:
-print(custom_sum.__doc__)  # 'This function can sum any objects which have __add___'
+print(custom_sum.__doc__)
+# 'This function can sum any objects which have __add___'
 print(custom_sum.__name__)  # 'custom_sum'
 print(custom_sum.__original_func)  # <function custom_sum at <some_id>>
 """
