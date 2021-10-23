b4fa5adcfabbf87ace8214c1de71a8c7e51fa93f
mishakras <irinabudtova59@gmail.com>
2021-10-23 16:21:03 +0300

    task 1 fix

--- homework/homework10/task1.py
+++ homework/homework10/task1.py
@@ -45,11 +45,12 @@ def make_string(tasks):
         if index > 10:
             break
         str1 += '{\n'
-        str1 += '\t"code": "' + i[1] + '"\n'
-        str1 += '\t"name": "' + i[0] + '"\n'
+        str1 += '\t"code": "' + i[1] + ',"\n'
+        str1 += '\t"name": "' + i[0] + ',"\n'
         str1 += '\t"' + str(i[2]) + '" |'
         str1 += ' "' + str(i[3]) + '" |'
-        str1 += ' "potential profit" : ' + str(i[4]) + ',\n'
+        str1 += ' "' + str(i[5]) + '" |'
+        str1 += ' "potential profit" : ' + str(i[4]) + '\n'
         str1 += '},\n'
     str1 = '[\n' + str1[:len(str1) - 2] + '\n]'
     return str1
@@ -68,15 +69,19 @@ async def start():
             tasks.append(i)
     tasks.sort(key=lambda x: x[2], reverse=True)
     str1 = make_string(tasks)
-    with open('../../price.json', 'w') as fi:
+    with open('price.json', 'w') as fi:
         fi.write(str1)
     tasks.sort(key=lambda x: x[3], reverse=True)
     str1 = make_string(tasks)
-    with open('p/e.json', 'w') as fi:
+    with open('pe.json', 'w') as fi:
         fi.write(str1)
     tasks.sort(key=lambda x: x[4], reverse=True)
     str1 = make_string(tasks)
     with open('dif.json', 'w') as fi:
+        fi.write(str1)
+    tasks.sort(key=lambda x: x[5], reverse=True)
+    str1 = make_string(tasks)
+    with open('growth.json', 'w') as fi:
         fi.write(str1)
 
 
@@ -97,19 +102,19 @@ async def main(i):
     return tasks
 
 
-def cost(temp):
-    while temp[0] == '\n' or temp[0] == '\t' or temp[0] == '\r':
-        temp = temp[1:]
-    while temp[len(temp) - 1] == '\n' or temp[len(temp) - 1] == '\t'\
-            or temp[len(temp) - 1] == '\r':
-        temp = temp[:len(temp) - 1]
-    if ',' in temp:
-        index = temp.find(',')
-        temp = temp[:index] + temp[index + 1:]
-    index = temp.find('.')
-    temp = temp[:index] + temp[index + 1:]
-    temp = int(temp) / 100
-    return temp
+def cost(int_in_str, mult):
+    while int_in_str[0] == '\n' or int_in_str[0] == '\t' or int_in_str[0] == '\r':
+        int_in_str = int_in_str[1:]
+    while int_in_str[len(int_in_str) - 1] == '\n' or int_in_str[len(int_in_str) - 1] == '\t'\
+            or int_in_str[len(int_in_str) - 1] == '\r':
+        int_in_str = int_in_str[:len(int_in_str) - 1]
+    if ',' in int_in_str:
+        index = int_in_str.find(',')
+        int_in_str = int_in_str[:index] + int_in_str[index + 1:]
+    index = int_in_str.find('.')
+    int_in_str = int_in_str[:index] + int_in_str[index + 1:]
+    int_in_str = int(int_in_str) / mult
+    return int_in_str
 
 
 def div(soup):
@@ -118,17 +123,17 @@ def div(soup):
         if 'class' in j.attrs:
             if ['snapshot__highlow-container'] == j['class']:
                 try:
-                    temp = j.contents[3].contents[1].contents[0]
-                    week_low = cost(temp)
-                    temp = j.contents[3].contents[3].contents[0]
-                    week_high = cost(temp)
-                    temp = (week_high-week_low)/week_low
+                    week_low = j.contents[3].contents[1].contents[0]
+                    week_low = cost(week_low, 100)
+                    week_high = j.contents[3].contents[3].contents[0]
+                    week_high = cost(week_high, 100)
+                    week_diff = (week_high-week_low)/week_low
                 except IndexError:
-                    temp = 0
+                    week_diff = 0
             elif ['snapshot__data-item'] == j['class']:
                 if 'Ratio' in str(j.contents[1].string):
-                    p_e = cost(str(j.contents[0].string))
-    return [p_e, temp]
+                    p_e = cost(str(j.contents[0].string), 100)
+    return [p_e, week_diff]
 
 
 def span(soup):
@@ -139,7 +144,7 @@ def span(soup):
             elif ['price-section__category'] == j['class']:
                 company_type = str(j.span.string)[2:]
             elif['price-section__current-value'] == j['class']:
-                price = cost(str(j.string))
+                price = cost(str(j.string),100)
     return [name, company_type, price]
 
 
@@ -151,7 +156,19 @@ async def company(href):
     soup = BeautifulSoup(html, 'html.parser')
     name, company_type, price = span(soup)
     week_diff, p_e = div(soup)
-    return [name, company_type, price, p_e, week_diff]
+    end_price = -100
+    start_price = 1
+    for j in soup.find_all('script'):
+        if 'window.historicalPrices = window.historicalPrices' in str(j.string):
+            for i in reversed(str(j.string).split(',')):
+                if 'Close' in i:
+                    end_price = cost(i.split(':')[1], 100000)
+                    break
+            for i in reversed(str(j.string).split(',')):
+                if 'Open' in i:
+                    start_price = cost(i.split(':')[1], 100000)
+                    break
+    return [name, company_type, price, p_e, week_diff, (end_price-start_price)/start_price]
 
 
 loop = asyncio.get_event_loop()
