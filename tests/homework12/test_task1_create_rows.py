644e5cd28205fcba07a15b2754acd2d5fcd95daf
mishakras <irinabudtova59@gmail.com>
2021-10-31 16:19:07 +0300

    test_task 1 fix

--- tests/homework12/test_task1_create_rows.py
+++ tests/homework12/test_task1_create_rows.py
@@ -3,7 +3,8 @@ from homework.homework12.task1_tables import (Homework, HomeworkResult,
                                               Student, Teacher)
 
 with task1.session_scope() as session:
-    student = task1.make_student('Michail', 'Kras')
+    student = task1.make_student('Alexander', 'Kras')
+    student2 = task1.make_student('Michail', 'Kras')
     teacher = task1.make_teacher('Daniil', 'Shadrin')
     homework = task1.make_homework('Read docs', 5)
     task1.make_homework_result(homework, student,
@@ -17,6 +18,24 @@ def test_read_student():
             if i.name == 'Michail':
                 return
     assert False
+
+
+def test_read_another_student():
+    with task1.session_scope() as session:
+        q = session.query(Student).all()
+        for i in q:
+            if i.name == 'Alexander':
+                return
+        assert False
+
+
+def test_read_wrong_student():
+    with task1.session_scope() as session:
+        q = session.query(Student).all()
+        for i in q:
+            if i.name == 'Daniil':
+                assert False
+        return
 
 
 def test_read_teacher():
